711fc44d9fada12f615aaa1e701f2dd7f9802269
Anurag Pandey <anuragpandey537755@gmail.com>
2025-08-20 13:46:38 +0530

    added Resume Download button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,9 @@
 import { motion } from "framer-motion";
 import profilePic from "../assets/Profile_picture.jpg";
 
+
 export default function Hero() {
+      
     return (
         <section
             id="home"
@@ -16,7 +18,7 @@ export default function Hero() {
                 className="md:w-1/2"
             >
                 <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
-                    Hi, I'm <span style={{color: 'red'}}>ANURAG PANDEY</span>
+                    Hi, I'm ANURAG PANDEY
                 </h1>
                 <p className="mt-4 font-serif text-xl text-gray-700 dark:text-gray-300">
                     Hi Everyone I am a frontend developer with a strong eye for design and detail, I specialize in building responsive, user-friendly web interfaces using modern technologies like HTML5, CSS3, JavaScript (ES6+), React.js, and TailwindCSS. My focus has always been on delivering clean, maintainable code and intuitive UI/UX that drive real user engagement.
@@ -26,6 +28,27 @@ export default function Hero() {
 
                     I enjoy solving real-world problems with efficient code and collaborating in cross-functional teams, and staying current with evolving web trends and technologies. My goal is to build scalable, performance, and impactful digital products from front to back.
                 </p>
+       <a href="/Resume.pdf" download>
+  <button
+    style={{
+        
+     border: "2px solid gray",
+      height: "60px",
+      width: "140px",
+      borderRadius: "15px",
+      fontFamily: 'sans-serif',
+      fontStyle: 'bold',
+      color:'red',
+      marginTop: "80px",
+      
+
+    }}
+    
+  >
+    DOWNLOAD RESUME
+  </button>
+</a>
+
             </motion.div>
 
             {/* Profile Image */}
@@ -40,6 +63,8 @@ export default function Hero() {
   className="w-90 md:w-72 lg:w-80 h-auto object-cover rounded-full border-4 border-gray-500 shadow-lg"
 />
             </motion.div>
+
+            
         </section>
     );
 }
